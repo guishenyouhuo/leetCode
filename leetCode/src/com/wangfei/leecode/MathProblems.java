c387cf45adc2785745d09bc8cdea082ec7420ee5
guishenyouhuo <fly_wang_fei@163.com>
2018-06-11 10:48:57 +0800

    20180608

--- leetCode/src/com/wangfei/leecode/MathProblems.java
+++ leetCode/src/com/wangfei/leecode/MathProblems.java
@@ -241,4 +241,32 @@ public class MathProblems {
         }
     	return num;
     }
+    
+    /**
+     * 给定一个长度为 n 的非空整数数组，找到让数组所有元素相等的最小移动次数。每次移动可以使 n - 1 个元素增加 1。
+		示例:
+		输入: [1,2,3]
+		输出: 3
+		解释:
+		只需要3次移动（注意每次移动会增加两个元素的值）：
+		[1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
+		
+		解法：此题需要找到规律   最小移动次数即为数组中每个元素减去最小数的和
+     * @param nums
+     * @return
+     */
+    public int minMoves(int[] nums) {
+     
+    	int min = Integer.MAX_VALUE;
+    	for (int num : nums) {
+			min = Math.min(num, min);
+		}
+    	int cnt = 0;
+    	for (int num : nums) {
+			if(num > min){
+				cnt += num - min;
+			}
+		}
+    	return cnt;
+    }
 }
