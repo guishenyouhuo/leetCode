8b54c601005f35764d52d0ffcdbc0a3c7900269c
guishenyouhuo <fly_wang_fei@163.com>
2018-11-12 23:23:56 +0800

    数组拆分1

---
+++ leetCode/src/com/wangfei/leecode/ArrayProcess.java
@@ -0,0 +1,20 @@
+package com.wangfei.leecode;
+
+import java.util.Arrays;
+
+public class ArrayProcess {
+    public static void main(String[] args) {
+
+        int[] nums = {1, 4, 3, 2};
+        System.out.println(arrayPairSum(nums));
+    }
+
+    public static int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int result = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            result += nums[i];
+        }
+        return result;
+    }
+}
